8dad1dc13b2557f648c69ef13e10387d9f16ae5c
saif-ullah-k <166329933+saif-ullah-k@users.noreply.github.com>
2025-09-19 03:34:24 +0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,14 +1,14 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const { password } = req.body;
-
-    if (password === process.env.PASSWORD) {
-      return res.status(200).json({ success: true });
-    } else {
-      return res.status(401).json({ success: false, message: "Unauthorized" });
-    }
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const { password } = req.body;
+
+  if (password === process.env.PASSWORD) {
+    return res.status(200).json({ success: true });
+  } else {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
   }
 }
